bf7e8809ae0e2796880543eda1b85266c27a8044
ehuntober <ehuntercrypto@gmail.com>
2023-06-01 06:09:27 +0000

    new commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const hbs = require('hbs')
+
+var app = express()
+
+app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/public'))
+
+app.get('/',(req,res) =>{
+    res.send({
+        name: 'kelechi',
+        like:[
+            'cities',
+            'buses'
+        ]
+    })
+})
+
+app.get('/about', (req,res) =>{
+    // res.send('about page')
+    res.render('about.hbs')
+})
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        errormessage: 'Unable to handle request'
+    })
+})
+app.listen(3000);
